9ed886e691f003c3754c57433d6ddf3f39f52539
LukeKope <luke.koppenheffer@nyu.edu>
2019-10-27 14:34:29 -0400

    Added concrete ideas and worked on getting frequencies to take up full screen

---
+++ Code/sketch.js
@@ -0,0 +1,55 @@
+let osc, env, fft;
+
+let attackLevel = 1.0;
+let releaseLevel = 0;
+
+let attackTime = 0.001;
+let decayTime = 0.2;
+let susPercent = 0.2;
+let releaseTime = 0.5;
+
+function setup() {
+  env = new p5.Envelope();
+  env.setADSR(attackTime, decayTime, susPercent, releaseTime);
+  env.setRange(attackLevel, releaseLevel);
+  //this is another way to make a sine wave oscillator
+  osc = new p5.Oscillator();
+  osc.amp(env);
+  //Set frequency to change based on level of game. Need to set a target frequency and then have game start at random frequency
+  osc.freq(440);
+  osc.setType("sine");
+  osc.start();
+
+  fft = new p5.FFT();
+}
+
+function draw() {
+  createCanvas(windowWidth, windowHeight);
+
+  let freqs = fft.analyze();
+
+  push();
+  noStroke();
+  fill(0, fft.getEnergy(440), 0); // spectrum is green depending on energy of the frequency. Have color change to red if further or blue if further. Maybe blue for too low and red for too high
+  for (let i = 0; i < freqs.length; i++) {
+    let x = map(i, 0, freqs.length, 0, windowWidth);
+    let h = -height + map(freqs[i], 0, 255, height, 0);
+    rect(x, height, windowWidth / freqs.length, h);
+  }
+  pop();
+
+  //Game instructionss
+  textSize(24);
+  text("'a' to play note", 20, 100);
+}
+
+function keyReleased() {
+  if (key == "a") {
+    env.triggerAttack();
+  }
+}
+
+//Mapping frequency to the position of the mouse
+function mouseDragged() {
+  osc.freq(map(mouseX, 0, windowWidth, 0, 2200));
+}
